dbb893b6c31dda18cdb5c9e0f53fe450d26ab576
thebenjieangeles <zerofourtwentifive@gmail.com>
2023-07-27 13:43:33 +0700

    initial routes created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import AddBooks from "./pages/AddBooks";
+import Books from "./pages/Books";
 import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Navbar />
-      <Home />
-    </div>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/" element={<Books />} />
+        <Route path="/" element={<AddBooks />} />
+      </Routes>
+    </Router>
   );
 }
 
